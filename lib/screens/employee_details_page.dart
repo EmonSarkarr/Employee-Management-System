69b872be4192bdba704b9c4170adfc39c6c8a422
EMON <emonsarkar1111@gmail.com>
2023-04-19 13:17:38 +0600

    details page

--- lib/screens/employee_details_page.dart
+++ lib/screens/employee_details_page.dart
@@ -1,8 +1,9 @@
+import 'package:employee_management_system/models/employee_model.dart';
 import 'package:flutter/material.dart';
 
 class EmployeeDetailsPage extends StatefulWidget {
-
   static const nameRouting = "/employeeDetailsPage";
+
   const EmployeeDetailsPage({Key? key}) : super(key: key);
 
   @override
@@ -11,23 +12,80 @@ class EmployeeDetailsPage extends StatefulWidget {
 
 class _EmployeeDetailsPageState extends State<EmployeeDetailsPage> {
   late Size size;
+  late EmployeeModel employee;
+@override
+  void didChangeDependencies() {
+    employee = ModalRoute.of(context)!.settings.arguments as EmployeeModel;
+    super.didChangeDependencies();
+  }
   @override
   Widget build(BuildContext context) {
     size = MediaQuery.of(context).size;
-    return SafeArea(child: Scaffold(
-      appBar: AppBar(title: const Text("Employee Details page"),centerTitle: true),
-      backgroundColor: Colors.cyan,
+    return SafeArea(
+        child: Scaffold(
+      appBar:
+          AppBar(title: const Text('Employee Details Page'), centerTitle: true),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Container(
-            height: size.height / 3,
-            width:double.infinity,
-            color: Colors.purple,
-            child: const Text("EMP image"),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.pink.shade100,
+                  borderRadius: BorderRadius.circular(20)),
+              height: size.height / 3,
+              width: double.infinity,
+              child: Center(child:  Text("EMP image ${employee.name}")),
+            ),
           ),
-
+           Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListTile(
+              hoverColor: Colors.red,
+                title: Text("NAME: ${employee.name}"),
+                trailing: IconButton(onPressed: (){},icon: const Icon(Icons.person)),
+            tileColor: Colors.black54),
+          ),
+           Padding(
+            padding:const EdgeInsets.all(8.0),
+            child: ListTile(
+              hoverColor: Colors.red,
+                trailing: IconButton(onPressed: (){},icon: const Icon(Icons.email)),
+                title: Text("Email: ${employee.email} "),
+            tileColor: Colors.black54),
+          ),
+           Padding(
+            padding:const EdgeInsets.all(8.0),
+            child: ListTile(
+              hoverColor: Colors.red,
+                trailing: IconButton(onPressed: (){},icon: const Icon(Icons.phone)),
+                title: Text("Phone: ${employee.phoneNumber} "),
+            tileColor: Colors.black54),
+          ),
+           Padding(
+            padding:const EdgeInsets.all(8.0),
+            child: ListTile(
+              hoverColor: Colors.red,
+                trailing: IconButton(onPressed: (){},icon: const Icon(Icons.monetization_on)),
+                title: Text("Salary: ${employee.salary} "),
+            tileColor: Colors.black54),
+          ),
+           Padding(
+            padding:const EdgeInsets.all(8.0),
+            child: ListTile(
+              hoverColor: Colors.red,
+                trailing: IconButton(onPressed: (){},icon: const Icon(Icons.info)),
+                title: Text("Designation: ${employee.designation} "),
+            tileColor: Colors.black54),
+          ),
+           Padding(
+            padding:const EdgeInsets.all(8.0),
+            child: ListTile(
+              hoverColor: Colors.red,
+                trailing: IconButton(onPressed: (){},icon: const Icon(Icons.location_on)),
+                title: Text("Address: ${employee.address} "),
+            tileColor: Colors.black54),
+          ),
         ],
       ),
     ));
--- lib/screens/employee_page.dart
+++ lib/screens/employee_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 
 class EmployeePage extends StatefulWidget {
   static const nameRouting = "/employeePage";
+
   const EmployeePage({Key? key}) : super(key: key);
 
   @override
@@ -16,33 +17,33 @@ class _EmployeePageState extends State<EmployeePage> {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-          floatingActionButton: FloatingActionButton(
-            onPressed: (){
-
-              Navigator.pushNamed(context, AddEmployeePage.nameRouting);
-            },
-            child: const Icon(Icons.add),
-          ),
-          body: ListView.builder(
-              itemCount: empList.length,
-              itemBuilder: (context,index){
-                final emp = empList[index];
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ListTile(
-                    title: Text(emp.name),
-                    subtitle: Text("emp.email"),
-                    trailing: Text("$index"),
-                    onTap: (){
-                      Navigator.pushNamed(context, EmployeeDetailsPage.nameRouting,arguments: emp);
-
-                    },
-                    leading: IconButton(onPressed: (){},icon: const Icon(Icons.person)),
-                    tileColor: index.isEven?Colors.orange:Colors.redAccent,
-                  ),
-                );
-              }),
-        ));
-
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushNamed(context, AddEmployeePage.nameRouting)
+              .then((value) => setState(() {}));
+        },
+        child: const Icon(Icons.add),
+      ),
+      body: ListView.builder(
+          itemCount: empList.length,
+          itemBuilder: (context, index) {
+            final emp = empList[index];
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                title: Text(emp.name),
+                subtitle: Text(emp.email),
+                trailing: Text("$index"),
+                onTap: () {
+                  Navigator.pushNamed(context, EmployeeDetailsPage.nameRouting,
+                      arguments: emp);
+                },
+                leading: IconButton(
+                    onPressed: () {}, icon: const Icon(Icons.person)),
+                tileColor: index.isEven ? Colors.orange : Colors.redAccent,
+              ),
+            );
+          }),
+    ));
   }
 }
