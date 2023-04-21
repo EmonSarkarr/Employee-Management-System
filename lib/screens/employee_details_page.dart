8245341cfb82377618a0e980b3dcc236efcbe67a
EMON <emonsarkar1111@gmail.com>
2023-04-21 10:52:04 +0600

    call and message worked in details page

--- lib/screens/employee_details_page.dart
+++ lib/screens/employee_details_page.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:employee_management_system/models/employee_model.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class EmployeeDetailsPage extends StatefulWidget {
   static const nameRouting = "/employeeDetailsPage";
@@ -52,21 +53,38 @@ class _EmployeeDetailsPageState extends State<EmployeeDetailsPage> {
                             fit: BoxFit.cover),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ListTile(
-                      hoverColor: Colors.red,
+                Container(
+                  padding: const EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Card(
+                    child: ListTile(
+                      contentPadding: const EdgeInsets.all(20),
+                      onTap: () {},
                       title: Text(
                         "NAME: ${employee.name}",
                         style: const TextStyle(fontSize: 20),
                       ),
-                      trailing: IconButton(
-                          onPressed: () {}, icon: const Icon(Icons.person)),
-                      tileColor: Colors.black54),
+                      trailing: Row(mainAxisSize: MainAxisSize.min, children: [
+                        IconButton(
+                            onPressed: _callEmployee, icon: const Icon(Icons.call)),
+                        IconButton(
+                            onPressed: _messageEmployee, icon: const Icon(Icons.message)),
+                      ]),
+                      // trailing: IconButton(
+                      //     onPressed: () {}, icon: const Icon(Icons.person)),
+                    ),
+                  ),
                 ),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: ListTile(
+                      contentPadding: EdgeInsets.all(15),
+                      onTap: () {},
+                      shape: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20)),
                       hoverColor: Colors.red,
                       trailing: IconButton(
                           onPressed: () {}, icon: const Icon(Icons.email)),
@@ -78,9 +96,7 @@ class _EmployeeDetailsPageState extends State<EmployeeDetailsPage> {
                   child: ListTile(
                       hoverColor: Colors.red,
                       trailing: IconButton(
-                          onPressed: () {
-                            print("Calling");
-                          },
+                          onPressed: _callEmployee,
                           icon: const Icon(Icons.phone)),
                       title: Text("Phone: ${employee.phoneNumber} "),
                       tileColor: Colors.black54),
@@ -88,6 +104,10 @@ class _EmployeeDetailsPageState extends State<EmployeeDetailsPage> {
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: ListTile(
+                      shape: const OutlineInputBorder(
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(20),
+                              topRight: Radius.circular(20))),
                       hoverColor: Colors.red,
                       trailing: IconButton(
                           onPressed: () {},
@@ -125,8 +145,26 @@ class _EmployeeDetailsPageState extends State<EmployeeDetailsPage> {
                           : Text("Date of birth: ${employee.dob} "),
                       tileColor: Colors.black54),
                 ),
-
               ],
             )));
+  }
+
+  void _callEmployee() async {
+    final uri = Uri.parse("tel:${employee.phoneNumber}");
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    } else {
+      throw 'could not lunch url';
+    }
+  }
+
+  void _messageEmployee() async {
+    final uri = Uri.parse("sms:${employee.phoneNumber}");
+    if (await canLaunchUrl(uri)) {
+    await launchUrl(uri);
+    } else {
+    throw 'could not lunch url';
+    }
+
   }
 }
